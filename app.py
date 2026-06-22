b205526bf7dd7e482da37ea89c3f67f25d52da00
CarlySo132 <stu24121@ashs.school.nz>
2026-06-23 11:40:11 +1200

    New pages + Card component
    I added card component where it redirects to the new pages

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
 @app.route("/")
 def index():
@@ -13,6 +21,14 @@ def rules():
 @app.route("/bracket")
 def bracket():
     return render_template('bracket.html')
+
+@app.route("/badminton")
+def badminton():
+    return render_template('badminton.html')
+
+@app.route("/football")
+def football():
+    return render_template('football.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
