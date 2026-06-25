f2e4ebcb363df8ca6700378962281abc991e8dd1
CarlySo132 <stu24121@ashs.school.nz>
2026-06-26 00:22:30 +1200

    Sqlalchemy stuff
    Database stuff

--- app.py
+++ app.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Bracket.db'
 db = SQLAlchemy(app)
 
-class Post(db.Model):
+class Bracket(db.Model):
+    __tablename__ = 'teams'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    seed = db.Column(db.Integer, nullable=False)
+
+class Match(db.Model):
+    __tablename__ = 'matches'
+    id = db.Column(db.Integer, primary_key=True)
+    round = db.Column(db.Integer, nullable=False)
+    match_index= db.Column(db.Integer, nullable=False)
+    team1_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    team2_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    winner_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=True)
+
+match = Match.query.get(1)
+team1 = Team.query.get(match.team1_id)
+team2 = Team.query.get(match.team2_id)
+
+print(team1.name, "vs", team2.name)
 
 @app.route("/")
 def index():
@@ -20,7 +36,9 @@ def rules():
 
 @app.route("/bracket")
 def bracket():
-    return render_template('bracket.html')
+    teams = Team.query.all()
+    matches = Match.query.order_by(Match.round, Match.match_index).all()
+    return render_template('bracket.html', teams=teams, matches=matches)
 
 @app.route("/badminton")
 def badminton():
