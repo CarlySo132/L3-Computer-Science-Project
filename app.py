a9ae1166dbe895333aed3d2da5af462dae857806
CarlySo132 <stu24121@ashs.school.nz>
2026-09-05 14:32:37 +1200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import string
 from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bracket.db'
 db = SQLAlchemy(app)
